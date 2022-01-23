c26cb780dce7203a298210385422dc259c5bfed7
casey <cassandra.d.mclellan@gmail.com>
2022-01-23 15:46:35 +0000

    CHORE: put About and Home components in their own folders

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,8 @@
 import { Routes, Route, Link } from "react-router-dom";
 
+import Home from "../Home";
+import About from "../About";
+
 import './App.css';
 
 function App() {
@@ -15,35 +18,3 @@ function App() {
 }
 
 export default App;
-
-// App.js
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>
-          That feels like an existential question, don't you
-          think?
-        </p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
