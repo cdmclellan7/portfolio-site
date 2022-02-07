020b53eb04623c555d6fe8540c7f2f0bcc36164e
casey <cassandra.d.mclellan@gmail.com>
2022-02-07 09:51:47 +0000

    FEAT: added logo links for contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,20 @@
+import githubLogo from "../../logos/GitHub-Mark-Light-64px.png";
+import linkedinLogo from "../../logos/linkedin-3-64.png";
+
 function Contact() {
-    return (
-        <>
-            <main>
-                <p>Contact Me</p>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <h1>Contact Me</h1>
+        <a href="https://www.linkedin.com/in/cassandra-mclellan-37b221231/">
+          <img src={linkedinLogo} alt="Linkedin logo"></img>
+        </a>
+        <a href="https://github.com/cdmclellan7" target="_blank">
+          <img src={githubLogo} alt="Github logo"></img>
+        </a>
+      </main>
+    </>
+  );
 }
 
 export default Contact;
