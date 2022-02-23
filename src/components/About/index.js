f30fb6993a445bfc8595bfeda1b2ed0b1bef37e3
casey <cassandra.d.mclellan@gmail.com>
2022-02-23 19:56:46 +0000

    FEAT: copied bio text from linkedin and added margins

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,10 +3,27 @@ function About() {
     <>
       <main>
         <h2>About Me</h2>
-        <p>Intro</p>
-        <p>School of Code</p>
-        <p>previous experience</p>
-        <p>closing</p>
+        <div className="about-page-container">
+          <p>
+            I am a full-stack software engineer currently studying with the
+            School of Code, a 16-week bootcamp that focuses on working in an
+            agile team, problem solving skills, and design thinking. From this
+            course I have experience in the latest web development technologies.
+            I am changing careers from English language teaching to tech.
+            Teaching requires clear communication skills, organization, and
+            planning, all of which are essential skills in software development.
+            My biggest strength is my passion for learning new things.
+          </p>
+          <p>
+            The School of Code is an intensive learning environment. Our core
+            stack is React, Node.js / Express, PostgreSQL, and testing with
+            Jest. We have also used TypeScript, Docker, Next.js, Cypress, Auth0,
+            and Storybook. To supplement my learning, I am pursuing the AWS
+            Cloud Practitioner Certification.
+          </p>
+          <p>previous experience</p>
+          <p>closing</p>
+        </div>
       </main>
     </>
   );
