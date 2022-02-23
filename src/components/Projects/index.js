c455eb7704a9dd548f28a50e758de3e42f23d0a0
casey <cassandra.d.mclellan@gmail.com>
2022-02-23 21:58:56 +0000

    FEAT: put dummy projects for all project sections

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -78,7 +78,151 @@ function Projects() {
         </div>
 
         <h2>Frontend Apps</h2>
+        <div className="projects-container">
+          <div>
+            {" "}
+            <a
+              href="https://in-the-l00p.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="project-title">In the Loop</p>
+            </a>
+            <p>
+              A messaging forum where Schoolf of code bootcampers can ask and
+              answer coding questions. The MVP was made for the School of Code's
+              1-week team project. I am continuing to add functionality.
+            </p>
+            <a
+              href="https://github.com/cdmclellan7/in-the-loop"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              See on Github
+            </a>
+            <ul>
+              <li>React</li>
+              <li>Node.js / Express</li>
+              <li>Postgres</li>
+              <li>Auth0 - user authentication</li>
+            </ul>
+          </div>
+          <div>
+            {" "}
+            <a
+              href="https://in-the-l00p.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="project-title">In the Loop</p>
+            </a>
+            <p>
+              A messaging forum where bootcampers can ask and answer tech
+              questions. The MVP was made for the School of Code's 1-week team
+              project. I am continuing to add functionality.
+            </p>
+            <ul>
+              <li>React</li>
+              <li>Node.js / Express</li>
+              <li>Postgres</li>
+              <li>Auth0 - user authentication</li>
+            </ul>
+          </div>
+          <div>
+            {" "}
+            <a
+              href="https://in-the-l00p.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="project-title">In the Loop</p>
+            </a>
+            <p>
+              A messaging forum where bootcampers can ask and answer tech
+              questions. The MVP was made for the School of Code's 1-week team
+              project. I am continuing to add functionality.
+            </p>
+            <ul>
+              <li>React</li>
+              <li>Node.js / Express</li>
+              <li>Postgres</li>
+              <li>Auth0 - user authentication</li>
+            </ul>
+          </div>
+        </div>
         <h2>Data Visualisations</h2>
+        <div className="projects-container">
+          <div>
+            {" "}
+            <a
+              href="https://in-the-l00p.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="project-title">In the Loop</p>
+            </a>
+            <p>
+              A messaging forum where Schoolf of code bootcampers can ask and
+              answer coding questions. The MVP was made for the School of Code's
+              1-week team project. I am continuing to add functionality.
+            </p>
+            <a
+              href="https://github.com/cdmclellan7/in-the-loop"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              See on Github
+            </a>
+            <ul>
+              <li>React</li>
+              <li>Node.js / Express</li>
+              <li>Postgres</li>
+              <li>Auth0 - user authentication</li>
+            </ul>
+          </div>
+          <div>
+            {" "}
+            <a
+              href="https://in-the-l00p.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="project-title">In the Loop</p>
+            </a>
+            <p>
+              A messaging forum where bootcampers can ask and answer tech
+              questions. The MVP was made for the School of Code's 1-week team
+              project. I am continuing to add functionality.
+            </p>
+            <ul>
+              <li>React</li>
+              <li>Node.js / Express</li>
+              <li>Postgres</li>
+              <li>Auth0 - user authentication</li>
+            </ul>
+          </div>
+          <div>
+            {" "}
+            <a
+              href="https://in-the-l00p.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="project-title">In the Loop</p>
+            </a>
+            <p>
+              A messaging forum where bootcampers can ask and answer tech
+              questions. The MVP was made for the School of Code's 1-week team
+              project. I am continuing to add functionality.
+            </p>
+            <ul>
+              <li>React</li>
+              <li>Node.js / Express</li>
+              <li>Postgres</li>
+              <li>Auth0 - user authentication</li>
+            </ul>
+          </div>
+        </div>
       </main>
     </>
   );
