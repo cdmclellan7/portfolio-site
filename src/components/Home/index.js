72418668704819798732e368f9e60d17cb29099d
casey <cassandra.d.mclellan@gmail.com>
2022-02-24 19:47:23 +0000

    FEAT: styled home page text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,11 +8,14 @@ function Home() {
           <span className="hello-world">Hello</span> Welcome to Casey's{" "}
           <span className="hello-world">World</span>
         </h1>
-        <p>
-          You've reached the profile site of me, Cassandra (Casey) McLellan. I'm
-          a fullstack engineer currently studying with the amazing School of
-          Code. I hope you enjoy your stay!
+        <p className="welcome-p">
+          You've reached the profile site of me, Cassandra (Casey) McLellan.{" "}
         </p>
+        <p className="welcome-p">
+          I'm a fullstack engineer currently studying with the amazing School of
+          Code.{" "}
+        </p>
+        <p className="welcome-p"> I hope you enjoy your visit here! :)</p>
       </main>
     </>
   );
