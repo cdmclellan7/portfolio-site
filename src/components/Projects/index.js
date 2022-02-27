02969433d1c91c654b640e97b2fcdc6abd898b48
casey <cassandra.d.mclellan@gmail.com>
2022-02-27 19:14:07 +0000

    FEAT: added codepen d3 data visualizations to project page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -15,7 +15,7 @@ function Projects() {
               <p className="project-title">In the Loop</p>
             </a>
             <p>
-              A messaging forum where Schoolf of code bootcampers can ask and
+              A messaging forum where School of code bootcampers can ask and
               answer coding questions. The MVP was made for the School of Code's
               1-week team project. I am continuing to add functionality.
             </p>
@@ -62,27 +62,6 @@ function Projects() {
               <li>Node.js / Express</li>
               <li>Postgres</li>
               <li>Passport.js / bcrypt - user authentication</li>
-            </ul>
-          </div>
-          <div className="project">
-            {" "}
-            <a
-              href="https://in-the-l00p.netlify.app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <p className="project-title">In the Loop</p>
-            </a>
-            <p>
-              A messaging forum where bootcampers can ask and answer tech
-              questions. The MVP was made for the School of Code's 1-week team
-              project. I am continuing to add functionality.
-            </p>
-            <ul>
-              <li>React</li>
-              <li>Node.js / Express</li>
-              <li>Postgres</li>
-              <li>Auth0 - user authentication</li>
             </ul>
           </div>
         </div>
@@ -160,77 +139,76 @@ function Projects() {
             </ul>
           </div>
         </div>
-        <h2>Data Visualisations</h2>
+        <h2>D3.js Data Visualisations</h2>
         <div className="projects-container">
           <div className="project">
-            {" "}
-            <a
-              href="https://in-the-l00p.netlify.app"
-              target="_blank"
-              rel="noopener noreferrer"
+            <iframe
+              height="454"
+              style={{ width: "100%" }}
+              scrolling="no"
+              title="FCC - D3 Tree Map"
+              src="https://codepen.io/stressstressstress/embed/PoZoLqL?default-tab=js%2Cresult"
+              frameborder="no"
+              loading="lazy"
+              allowtransparency="true"
+              allowfullscreen="true"
             >
-              <p className="project-title">In the Loop</p>
-            </a>
-            <p>
-              A messaging forum where Schoolf of code bootcampers can ask and
-              answer coding questions. The MVP was made for the School of Code's
-              1-week team project. I am continuing to add functionality.
-            </p>
-            <a
-              href="https://github.com/cdmclellan7/in-the-loop"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              See on Github
-            </a>
-            <ul>
-              <li>React</li>
-              <li>Node.js / Express</li>
-              <li>Postgres</li>
-              <li>Auth0 - user authentication</li>
-            </ul>
+              See the Pen{" "}
+              <a href="https://codepen.io/stressstressstress/pen/PoZoLqL">
+                FCC - D3 Tree Map
+              </a>{" "}
+              by Casey (
+              <a href="https://codepen.io/stressstressstress">
+                @stressstressstress
+              </a>
+              ) on <a href="https://codepen.io">CodePen</a>.
+            </iframe>
           </div>
           <div className="project">
-            {" "}
-            <a
-              href="https://in-the-l00p.netlify.app"
-              target="_blank"
-              rel="noopener noreferrer"
+            <iframe
+              height="454"
+              style={{ width: "100%" }}
+              scrolling="no"
+              title="FCC - D3 Choropleth Map"
+              src="https://codepen.io/stressstressstress/embed/LYEoBgO?default-tab=js%2Cresult"
+              frameborder="no"
+              loading="lazy"
+              allowtransparency="true"
+              allowfullscreen="true"
             >
-              <p className="project-title">In the Loop</p>
-            </a>
-            <p>
-              A messaging forum where bootcampers can ask and answer tech
-              questions. The MVP was made for the School of Code's 1-week team
-              project. I am continuing to add functionality.
-            </p>
-            <ul>
-              <li>React</li>
-              <li>Node.js / Express</li>
-              <li>Postgres</li>
-              <li>Auth0 - user authentication</li>
-            </ul>
+              See the Pen{" "}
+              <a href="https://codepen.io/stressstressstress/pen/LYEoBgO">
+                FCC - D3 Choropleth Map
+              </a>{" "}
+              by Casey (
+              <a href="https://codepen.io/stressstressstress">
+                @stressstressstress
+              </a>
+              ) on <a href="https://codepen.io">CodePen</a>.
+            </iframe>
           </div>
           <div className="project">
-            {" "}
-            <a
-              href="https://in-the-l00p.netlify.app"
-              target="_blank"
-              rel="noopener noreferrer"
+            <iframe
+              height="454"
+              style={{ width: "100%" }}
+              scrolling="no"
+              title="FCC - D3 Heat Map"
+              src="https://codepen.io/stressstressstress/embed/WNbqmBW?default-tab=js%2Cresult"
+              frameborder="no"
+              loading="lazy"
+              allowtransparency="true"
+              allowfullscreen="true"
             >
-              <p className="project-title">In the Loop</p>
-            </a>
-            <p>
-              A messaging forum where bootcampers can ask and answer tech
-              questions. The MVP was made for the School of Code's 1-week team
-              project. I am continuing to add functionality.
-            </p>
-            <ul>
-              <li>React</li>
-              <li>Node.js / Express</li>
-              <li>Postgres</li>
-              <li>Auth0 - user authentication</li>
-            </ul>
+              See the Pen{" "}
+              <a href="https://codepen.io/stressstressstress/pen/WNbqmBW">
+                FCC - D3 Heat Map
+              </a>{" "}
+              by Casey (
+              <a href="https://codepen.io/stressstressstress">
+                @stressstressstress
+              </a>
+              ) on <a href="https://codepen.io">CodePen</a>.
+            </iframe>
           </div>
         </div>
       </main>
